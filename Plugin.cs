eef22570e6a1ab20ce68b004352cf922348ebbb1
ConnorBieszk <147281703+pylua-dot-code@users.noreply.github.com>
2023-12-17 18:30:06 -0600

    Removed FPS and fixed issues.

--- Plugin.cs
+++ Plugin.cs
@@ -2,27 +2,13 @@
 using ComputerPlusPlus;
 using GorillaNetworking;
 using Photon.Pun;
+using System;
 using UnityEngine;
 
 [BepInPlugin("com.pyluadotcode.gorillatag.StatusScreen", "StatusScreen", "1.0.0")]
 [BepInDependency("com.kylethescientist.gorillatag.computerplusplus")]
-public class FrameChecker : BaseUnityPlugin 
-{
-    private static int FrameRate;
-    public static int fps
-    {
-        get { return FrameRate; }
-        set { FrameRate = value; }
-
-    }
-
-    public void Update()
-    {
-        float current = 0;
-        current = Time.frameCount / Time.time;
-        
-    }
-}
+public class Plugin : BaseUnityPlugin 
+{}
 
 public class Status : IScreen
 {
@@ -34,25 +20,26 @@ public class Status : IScreen
 
     public string Description => "This is a simple mod that adds a Status Page. This mod was created by pylua.code";
 
+    //public int Frames;
 
     public string GetContent()
     {
         
         if (PhotonNetwork.InRoom)
         {
+            // Frames = (int)Math.Round(1.0f / Time.unscaledDeltaTime);
             name = "Name: " + GorillaComputer.instance.currentName + "\n";
             string room = "Room: " + PhotonNetwork.CurrentRoom.Name + "\n";
             string roomplay = "Player Count: " + PhotonNetwork.CurrentRoom.PlayerCount + "\n";
             string gmod = "Game Mode: " + GorillaComputer.instance.currentGameMode + "\n";
             string master = "Master Client: " + PhotonNetwork.MasterClient + "\n";
             string color = "Color Code: " + (Color32)GorillaTagger.Instance.offlineVRRig.playerColor + "\n";
-            string fps = "FPS: " + FrameChecker.fps + "\n";
             if (gmod == "Game Mode: MODDED_MODDED_CASUALCASUAL\n")
                 gmod = "Game Mode: Modded_Casual\n";
-            Result = name + room + roomplay + gmod + master;
+            Result = name + room + roomplay + gmod + master + color;
         }
         else
-            Result = ("Name: " + GorillaComputer.instance.currentName + "\n" + "Color Code: " + (Color32)GorillaTagger.Instance.offlineVRRig.playerColor + "\n" + "FPS: " + FrameChecker.fps);
+            Result = ("Name: " + GorillaComputer.instance.currentName + "\n" + "Color Code: " + (Color32)GorillaTagger.Instance.offlineVRRig.playerColor + "\n");
         return (Result);
     }
 
