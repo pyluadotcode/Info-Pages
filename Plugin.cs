327ddb66bf624d7495580469ea47c5ba3054605b
ConnorBieszk <147281703+pylua-dot-code@users.noreply.github.com>
2023-12-17 18:18:08 -0600

    Created FPS and COLOR CODE sections

--- Plugin.cs
+++ Plugin.cs
@@ -2,12 +2,30 @@
 using ComputerPlusPlus;
 using GorillaNetworking;
 using Photon.Pun;
+using UnityEngine;
 
 [BepInPlugin("com.pyluadotcode.gorillatag.StatusScreen", "StatusScreen", "1.0.0")]
 [BepInDependency("com.kylethescientist.gorillatag.computerplusplus")]
-public class Plugin : BaseUnityPlugin { }
+public class FrameChecker : BaseUnityPlugin 
+{
+    private static int FrameRate;
+    private static int FPS;
+    public static int fps
+    {
+        get { return FrameRate; }
+        set { FrameRate = value; }
 
-public class TestScreen : IScreen
+    }
+
+    public void Update()
+    {
+        float current = 0;
+        current = Time.frameCount / Time.time;
+        
+    }
+}
+
+public class Status : IScreen
 {
     public string name = "";
 
@@ -17,8 +35,10 @@ public class TestScreen : IScreen
 
     public string Description => "This is a simple mod that adds a Status Page. This mod was created by pylua.code";
 
+
     public string GetContent()
     {
+        
         if (PhotonNetwork.InRoom)
         {
             name = "Name: " + GorillaComputer.instance.currentName + "\n";
@@ -26,6 +46,8 @@ public class TestScreen : IScreen
             string roomplay = "Player Count: " + PhotonNetwork.CurrentRoom.PlayerCount + "\n";
             string gmod = "Game Mode: " + GorillaComputer.instance.currentGameMode + "\n";
             string master = "Master Client: " + PhotonNetwork.MasterClient + "\n";
+            string color = "Color Code: " + GorillaTagger.Instance.offlineVRRig.playerColor + "\n";
+            string fps = "FPS: " + FrameChecker.fps + "\n";
             if (gmod == "Game Mode: MODDED_MODDED_CASUALCASUAL\n")
                 gmod = "Game Mode: Modded_Casual\n";
             Result = name + room + roomplay + gmod + master;
